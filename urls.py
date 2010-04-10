26e2a6e132d7f9670105860f759b929f3310dd3d
Chris McDonald <XWraithanX@gmail.com>
2010-04-10 08:33:17 -0700

    Setup media serving and moved CSS to external file.

--- urls.py
+++ urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls.defaults import *
 from django.contrib import admin
 import dselector
@@ -13,3 +14,8 @@ urlpatterns = parser.patterns('',
     (r'admin/doc/!', include('django.contrib.admindocs.urls')),
     (r'admin/(.*)!', admin.site.root),
 )
+
+if settings.DEBUG:
+    urlpatterns += parser.patterns('',
+        (r'media/{path:any}', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )
